aa3660099b61f2acb12965c205a4b84a09c89ea7
adam <adam@.gmail.com>
2023-05-19 12:52:31 -0700

    execution cd

--- utils.h
+++ utils.h
@@ -7,10 +7,12 @@
 #include <sys/wait.h>
 #include "main.h"
 
+extern char buff[1024];
+
 char **parse_command(char *command);
 void print_error(char *argVector, int count, char *command);
 int exec_command(char **command);
 int execute_ext_cmd(char *command, char **args);
-char *getDir(char *buff);
+char *getDir(void);
 
 #endif
