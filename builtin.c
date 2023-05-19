3d90f6471a427d6c35746aa5c2b18e55a9b872c5
adam <adam@.gmail.com>
2023-05-19 04:08:32 -0700

    Add env funct 	Add env()

--- builtin.c
+++ builtin.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
+extern char **environ;
 /**
  * cd - changes the current working directory
  * Return: 0 on success, 1 on failure
  */
 int cd(void)
 {
+	/* To take arguments from util.c file I'll add new function that
+	   takes them and return it to cd */
+	/* Argument that I want is name of the directory that I want to change
+	   to it */
 	printf("Changed Directories.\n");
 	return (0);
 }
@@ -25,6 +30,14 @@ int history(void)
   */
 int env(void)
 {
+	char **env = environ;
+
+	while (*env)
+	{
+		write(1, *env, strlen(*env));
+		write(1, "\n", 1);
+		env++;
+	}
 	return (0);
 }
 
