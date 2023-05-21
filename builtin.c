518e85b05b43d93067d1da70742408ab58a9e4f6
adam <adam@.gmail.com>
2023-05-21 13:23:03 -0700

    Add _strtok 	Add our strtok function

--- builtin.c
+++ builtin.c
@@ -107,3 +107,51 @@ int pwd(void)
 	free(buff);
 	return (0);
 }
+extern char *p;
+
+char *_strlite(char *str, const char *delim)
+{
+	int j = 0, i = 0, stop = 0;
+
+	while (str[j])
+	{
+		if (str[j] == '\0')
+			break;
+		for (i = 0; i < strlen(delim); i++)
+		{
+			if (str[j] == delim[i])
+			{
+				str[j] = '\0';
+				stop = 1;
+				break;
+			}
+		}
+		if (stop == 1)
+			break;
+		j++;
+	}
+	return (str);
+}
+char *_strtok(char *str, const char *delim)
+{
+	int i = 0;
+	char *p;
+
+	if (str = NULL)
+	{
+		while (p[i])
+		{
+			if (p[i] == '\0')
+			{
+				if (p[i + 1] == NULL)
+					str = NULL;
+				else
+					str = &p[i + 1];
+				break;
+			}
+			i++;
+		}
+	}
+	p = _strlite(str, delim);
+	return (p);
+}
