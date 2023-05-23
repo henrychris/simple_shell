300f58458c5e56a1cde0505be9d46805baf471ba
adam <adam@.gmail.com>
2023-05-23 09:24:38 -0700

    Try to fix memory leaks 	When I press exit or Ctr+D it still in the program

--- main.c
+++ main.c
@@ -9,18 +9,24 @@
 int main(__attribute__((unused))int argc, char **argv)
 {
 	char *command = NULL, **cmds;
-	int i = 0, count = 1, ret = 0;
+	int count = 1, ret = 0, j = 0;
 	size_t n = 0;
 
 	while (1)
 	{
 		printf("$ ");
-		i = getline(&command, &n, stdin);
-		if (strcmp(command, "exit\n") == 0 || i == EOF)
+		if (getline(&command, &n, stdin) <= -1)
 		{
-			if (i == -1)
-				write(1, "\n", 1);
-			break;
+			free(command);
+			command = NULL;
+			write(1, "\n", 1);
+			return (0);
+		}
+		if (strcmp(command, "exit\n") == 0)
+		{
+			free(command);
+			command = NULL;
+			return (0);
 		}
 
 		cmds = parse_command(command);
@@ -28,9 +34,25 @@ int main(__attribute__((unused))int argc, char **argv)
 		/* -1 = command not found */
 		if (ret == -1)
 			print_error(argv[0], count, command);
+		/* freeing command */
+		n = 0;
+		if (command != NULL)
+		{
+			free(command);
+			command = NULL;
+		}
+		/* freeing cmds */
+		for (j = 0; cmds[j] != NULL; j++)
+		{
+			free(cmds[j]);
+			cmds[j] = NULL;
+		}
+		if (cmds != NULL)
+		{
+			free(cmds);
+			cmds = NULL;
+		}
 		count++;
 	}
-	free(command);
-	free(cmds);
 	return (0);
 }
