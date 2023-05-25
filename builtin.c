6e95a7b212fde31d4cfff7ebe05b44d1eb0baa2a
Henry Ihenacho <henrychris2003@gmail.com>
2023-05-25 23:07:41 +0100

    betty checks

--- builtin.c
+++ builtin.c
@@ -1,74 +1,64 @@
-// #include "main.h"
-// #include "utils.h"
-
-// /**
-//  * cd - changes the current working directory
-//  * @args: args
-//  * Return: 0 on success, 1 on failure
-//  */
-// int cd(char **args)
-// {
-// 	char *dirName = args[1];
-
-// 	if (dirName == NULL)
-// 	{
-// 		dirName = getenv("HOME");
-// 		if (!dirName)
-// 			perror("Fail");
-// 	}
-// 	else if (strcmp(dirName, "-") == 0)
-// 	{
-// 		dirName = getenv("OLDPWD");
-// 		if (!dirName)
-// 			perror("Fail");
-// 	}
-// 	if (chdir(dirName) != 0)
-// 		perror("Fail");
-// 	else
-// 	{
-// 		if (setenv("PWD", dirName, 1) != 0)
-// 			perror("Fail");
-// 	}
-// 	return (0);
-// }
-
-// /**
-//  * env - get the environment
-//  * @args: args
-//  * Return: 0 or 1
-//  */
-// int env(__attribute__((unused))char **args)
-// {
-// 	char **env = environ;
-
-// 	while (*env)
-// 	{
-// 		write(1, *env, strlen(*env));
-// 		write(1, "\n", 1);
-// 		env++;
-// 	}
-// 	return (0);
-// }
-
-// /**
-//  * pwd - print the current directory
-//  * @args: args
-//  * Return: 0 or 1
-//  */
-// int pwd(__attribute__((unused))char **args)
-// {
-// 	char *buff = malloc(sizeof(char) * PATH_MAX);
-
-// 	if (getcwd(buff, PATH_MAX) == NULL)
-// 	{
-// 		perror("Error with getcwd() in pwd(void)");
-// 		free_ptr(&buff);
-// 		return (-2);
-// 	}
-
-// 	write(1, buff, strlen(buff));
-// 	write(1, "\n", 1);
-
-// 	free_ptr(&buff);
-// 	return (0);
-// }
+/*
+ *#include "main.h"
+ *#include "utils.h"
+ *
+ *
+ *int cd(char **args)
+ *{
+ *	char *dirName = args[1];
+ *
+ *	if (dirName == NULL)
+ *	{
+ *		dirName = getenv("HOME");
+ *		if (!dirName)
+ *			perror("Fail");
+ *	}
+ *	else if (strcmp(dirName, "-") == 0)
+ *	{
+ *		dirName = getenv("OLDPWD");
+ *		if (!dirName)
+ *			perror("Fail");
+ *	}
+ *	if (chdir(dirName) != 0)
+ *		perror("Fail");
+ *	else
+ *	{
+ *		if (setenv("PWD", dirName, 1) != 0)
+ *			perror("Fail");
+ *	}
+ *	return (0);
+ *}
+ *
+ *
+ *int env(__attribute__((unused))char **args)
+ *{
+ *	char **env = environ;
+ *
+ *	while (*env)
+ *	{
+ *		write(1, *env, strlen(*env));
+ *		write(1, "\n", 1);
+ *		env++;
+ *	}
+ *	return (0);
+ *}
+ *
+ *
+ *int pwd(__attribute__((unused))char **args)
+ *{
+ *	char *buff = malloc(sizeof(char) * PATH_MAX);
+ *
+ *	if (getcwd(buff, PATH_MAX) == NULL)
+ *	{
+ *		perror("Error with getcwd() in pwd(void)");
+ *		free_ptr(&buff);
+ *		return (-2);
+ *	}
+ *
+ *	write(1, buff, strlen(buff));
+ *	write(1, "\n", 1);
+ *
+ *	free_ptr(&buff);
+ *	return (0);
+ *}
+ */
--- free.c
+++ free.c
@@ -1,35 +1,31 @@
-// #include "utils.h"
-
-// /**
-//   * free_double_ptr - free **ptr
-//   * @ptr: ptr
-//   */
-// void free_double_ptr(char ***ptr)
-// {
-// 	int i;
-// 	char **p = *ptr;
-
-// 	if (*ptr != NULL)
-// 	{
-// 		for (i = 0; p[i] != NULL; i++)
-// 		{
-// 			free(p[i]);
-// 			p[i] = NULL;
-// 		}
-// 		free(*ptr);
-// 		*ptr = NULL;
-// 	}
-// }
-
-// /**
-//   * free_ptr - free *ptr
-//   * @command_ptr: command_ptr
-//   */
-// void free_ptr(char **command_ptr)
-// {
-// 	if (*command_ptr != NULL)
-// 	{
-// 		free(*command_ptr);
-// 		*command_ptr = NULL;
-// 	}
-// }
+/*
+ *#include "utils.h"
+ *
+ *
+ *void free_double_ptr(char ***ptr)
+ *{
+ *	int i;
+ *	char **p = *ptr;
+ *
+ *	if (*ptr != NULL)
+ *	{
+ *		for (i = 0; p[i] != NULL; i++)
+ *		{
+ *			free(p[i]);
+ *			p[i] = NULL;
+ *		}
+ *		free(*ptr);
+ *		*ptr = NULL;
+ *	}
+ *}
+ *
+ *
+ *void free_ptr(char **command_ptr)
+ *{
+ *	if (*command_ptr != NULL)
+ *	{
+ *		free(*command_ptr);
+ *		*command_ptr = NULL;
+ *	}
+ *}
+ */
